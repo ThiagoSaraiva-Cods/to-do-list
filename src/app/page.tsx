4b2c5fee44d5f9bf07055dc2030bcf417725b1b0
ThiagoSaraiva-Cods <thiagosaraiva.cods@gmail.com>
2025-09-27 15:24:45 -0300

    feat: implement task management with form validation and API integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,120 @@
-import { TaskList } from "@/components/task-list";
+"use client";
+
+import { TaskList, TaskProps } from "@/components/task-list";
+
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../components/input";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/button";
+
+const formSchema = z.object({
+  taskTitle: z.string().trim().min(1, "O título da tarefa é obrigatório."),
+});
 
 export default function Home() {
-  const tasks = [{
-    id: 1,
-    title: 'Task 1',
-    completed: false,
-  }, {
-    id: 2,
-    title: 'Task 2',
-    completed: true,
-  }]
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: zodResolver(formSchema),
+  });
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const response = await fetch("/api/tasks");
+      const data = await response.json();
+
+      setTasks(data);
+    };
+
+    getTasks();
+  }, []);
+
+  const handleCreateTask = async (data: z.infer<typeof formSchema>) => {
+    const response = await fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: data.taskTitle }),
+    });
+
+    const savedTask = await response.json();
+
+    setTasks((currentTasks) => [...currentTasks, savedTask.newTask]);
+
+    reset();
+  };
+
+  const handleToggleComplete = async (id: string) => {
+    const response = await fetch(`/api/tasks/${id}`, {
+      method: "PATCH",
+    });
+
+    if (!response.ok) {
+      console.error("Falha ao atualizar a tarefa.");
+      return;
+    }
+
+    const updatedTask = await response.json();
+
+    setTasks((currentTasks) =>
+      currentTasks.map((task) => (task.id === id ? updatedTask : task)),
+    );
+  };
+
+  const handleDeleteTask = async (id: string) => {
+    const response = await fetch(`/api/tasks/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      console.error("Falha ao excluir a tarefa.");
+      return;
+    }
+
+    setTasks((currentTasks) => currentTasks.filter((task) => task.id !== id));
+  };
 
   return (
-   <div className="p-5">
-    <TaskList tasks={tasks} />
-   </div>
+    <div className="mx-auto max-w-2xl p-5">
+      <form
+        onSubmit={handleSubmit(handleCreateTask)}
+        noValidate
+        className="flex flex-col flex-wrap gap-2 space-x-2 md:flex-nowrap"
+      >
+        <div className="flex items-center gap-2">
+          <Input
+            type="text"
+            {...register("taskTitle")}
+            name="taskTitle"
+            placeholder="Nova tarefa"
+            className="w-full"
+          />
+
+          <Button type="submit" className="h-[40px] min-w-fit">
+            Adicionar tarefa
+          </Button>
+        </div>
+        {errors.taskTitle && (
+          <p className="mt-1 text-sm text-red-500">
+            {errors.taskTitle.message}
+          </p>
+        )}
+      </form>
+      <div className="py-5">
+        <TaskList
+          tasks={tasks}
+          onToggleComplete={handleToggleComplete}
+          onDeleteTask={handleDeleteTask}
+        />
+      </div>
+    </div>
   );
 }
--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -1,25 +1,55 @@
-"use client"
-
-import { TaskItem, TaskProps } from "./task-item"
-import {useState} from 'react';
+import { Trash } from "lucide-react";
+import { Button } from "./button";
 
 interface TaskListProps {
-    tasks: TaskProps[];
+  tasks: TaskProps[];
+  onToggleComplete: (taskId: string) => void;
+  onDeleteTask: (taskId: string) => void;
+  className?: string;
 }
 
-export const TaskList = ({tasks}: TaskListProps) => {
-    const [isCompletedTask, setIsCompletedTask] = useState(false)
-
-    const handleToggleComplete = () => {
-        setIsCompletedTask(!isCompletedTask)
-    }
-    
-    return (
-        <div className={`flex flex-col gap-4`}>
-            {tasks.map(task => (
-                <TaskItem key={task.id} task={task} onToggleComplete={handleToggleComplete}/>
-            ))}
-        </div>
-    )
+export interface TaskProps {
+  id: string;
+  title: string;
+  completed: boolean;
 }
 
+export const TaskList = ({
+  tasks,
+  onToggleComplete,
+  onDeleteTask,
+  className,
+}: TaskListProps) => {
+  return (
+    <div className="flex flex-col gap-4">
+      {tasks.length > 0 ? (
+        tasks.map((task) => (
+          <div
+            key={task.id}
+            className="flex w-full justify-between gap-2 rounded-md border border-purple-700 p-2"
+          >
+            <div className="flex items-center gap-2 truncate rounded-md p-2">
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => onToggleComplete(task.id)}
+              />
+              <p
+                className={`text-md truncate ${task.completed ? "text-gray-500 line-through" : ""} ${className}`}
+              >
+                {task.title}
+              </p>
+            </div>
+            <Button onClick={() => onDeleteTask(task.id)} className="w-fit">
+              <Trash />
+            </Button>
+          </div>
+        ))
+      ) : (
+        <p className="text-md text-center text-gray-500">
+          Nenhuma tarefa encontrada!
+        </p>
+      )}
+    </div>
+  );
+};
